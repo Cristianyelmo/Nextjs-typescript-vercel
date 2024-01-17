b9f49b59ab2de52f6d87a6908036aad7ae7e7b11
Cristianyelmo <cristianama2019@gmail.com>
2024-01-17 13:48:08 -0300

    data layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,9 @@
 import NavBar from "./Navbar";
-
-export default function Layout({children}){
+import React, { ReactNode } from "react";
+interface LayoutProps {
+    children: ReactNode;
+  }
+export default function Layout({ children }: LayoutProps){
     return(
         <div>
 
